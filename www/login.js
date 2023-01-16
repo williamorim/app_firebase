d2983a2b6dbb632d8b502745d89ffbfe14c6d5b8
williamorim <thewilliam89@gmail.com>
2023-01-16 16:25:03 -0300

    colocando redirect

--- www/login.js
+++ www/login.js
@@ -3,7 +3,7 @@ login = function() {
   var password = document.getElementById("password").value;
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then(function(user){
-      alert("Logado com sucesso");
+      location.replace("http://127.0.0.1:4242/?t=true")
       auth = user;
     }).catch(function(error){
       alert("Falha ao logar");
